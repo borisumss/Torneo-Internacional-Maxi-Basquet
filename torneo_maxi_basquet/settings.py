aa05058039583c4aef7b4faeedd0a1ba8478ac61
Boris Calli <201900823@est.umss.edu>
2022-10-25 22:23:15 -0400

    deploy para railway

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-305_+qfegnqpy8o&099&m*b^)iuy*7+%v3(s3ppbs4%&2a!26%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
 # Application definition
@@ -120,8 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=['static/']
-
+#STATICFILES_DIRS=['static/']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
