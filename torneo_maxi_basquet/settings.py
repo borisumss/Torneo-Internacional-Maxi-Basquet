db1f41ddf9e8439858ef71fb9a98e888d6d1b1df
Boris Calli <201900823@est.umss.edu>
2022-10-30 15:22:13 -0400

    instalacion del modulo whitenoise

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #STATICFILES_DIRS=['static/']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+#####
+#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
