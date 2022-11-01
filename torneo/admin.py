e083256bf969f0e944eae939be52a77f61b6ca3d
Jheison Leon <201905980@est.umss.edu>
2022-11-01 19:01:02 -0400

    fix users

--- torneo/admin.py
+++ torneo/admin.py
@@ -1,7 +1,6 @@
 from ast import Or
 from django.contrib import admin
-from .models import Torneo, Inscripcion, Pre_Inscripcion,Categorias_Torneo, Organizador, Equipo, Entrenador, Jugador, Delegado
-
+from .models import Torneo, Inscripcion, Pre_Inscripcion,Categorias_Torneo, Organizador, Equipo, Entrenador, Jugador, Delegado, delegado_Inscripcion, delegado_PreInscripcion 
 # Register your models here.
 admin.site.register(Torneo)
 admin.site.register(Categorias_Torneo)
@@ -12,3 +11,5 @@ admin.site.register(Jugador)
 admin.site.register(Delegado)
 admin.site.register(Inscripcion)
 admin.site.register(Pre_Inscripcion)
+admin.site.register(delegado_Inscripcion)
+admin.site.register(delegado_PreInscripcion)
