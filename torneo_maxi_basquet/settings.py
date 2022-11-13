080d61f1a3df6a067fba9b08ef671faac7660b8c
borisumss <93559073+borisumss@users.noreply.github.com>
2022-11-13 14:45:17 -0400

    Update settings.py

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -117,8 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+##### DESARROLLO 
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS = ['static/']
+######
+
+##### DEPLOY
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#######
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
