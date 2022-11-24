ff613ec6baea3d915672c140f229b03afcd6a86b
cristian <crisarandosyse@gmail.com>
2022-11-24 00:40:45 -0400

    Add admin model views enhancement

--- torneo/admin.py
+++ torneo/admin.py
@@ -1,6 +1,5 @@
-from ast import Or
 from django.contrib import admin
-from .models import  Estadisticas_enfrentamiento,Enfrentamiento, Tabla_posiciones ,Torneo, Inscripcion, Pre_Inscripcion,Categorias_Torneo, Organizador, Equipo, Entrenador, Jugador, Delegado, delegado_Inscripcion, delegado_PreInscripcion 
+from .models import *
 # Register your models here.
 # admin.site.register(Torneo)
 # admin.site.register(Categorias_Torneo)
@@ -16,43 +15,67 @@ from .models import  Estadisticas_enfrentamiento,Enfrentamiento, Tabla_posicione
 # admin.site.register(Estadisticas_enfrentamiento)
 # admin.site.register(Enfrentamiento)
 
+
 @admin.register(Torneo)
 class Torneo(admin.ModelAdmin):
-    list_display = ("nombre_torneo", "fecha_torneo_inicio", "fecha_torneo_fin")
-    
+    list_display = [f.name for f in Torneo._meta.fields]
+
+
 @admin.register(Organizador)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Organizador._meta.fields]
+
+
 @admin.register(Inscripcion)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Inscripcion._meta.fields]
+
+
 @admin.register(Pre_Inscripcion)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Pre_Inscripcion._meta.fields]
+
+
 @admin.register(delegado_Inscripcion)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in delegado_Inscripcion._meta.fields]
+
+
 @admin.register(delegado_PreInscripcion)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in delegado_PreInscripcion._meta.fields]
+
+
 @admin.register(Categorias_Torneo)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Categorias_Torneo._meta.fields]
+
+
 @admin.register(Entrenador)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Entrenador._meta.fields]
+
+
 @admin.register(Equipo)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Equipo._meta.fields]
+
+
 @admin.register(Jugador)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Jugador._meta.fields]
+
+
 @admin.register(Tabla_posiciones)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Tabla_posiciones._meta.fields]
+
+
 @admin.register(Enfrentamiento)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Enfrentamiento._meta.fields]
+
+
 @admin.register(Estadisticas_enfrentamiento)
 class Torneo(admin.ModelAdmin):
-    pass
+    list_display = [f.name for f in Estadisticas_enfrentamiento._meta.fields]
