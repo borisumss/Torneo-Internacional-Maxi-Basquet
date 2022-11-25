1dd7d0f47d0469faaa6bbcb12b87099bc638b0d7
Jheison Leon <201905980@est.umss.edu>
2022-11-25 19:53:21 -0400

    correo

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -145,12 +145,12 @@ EMAIL_USE_TLS = True
 
 ### DEPLOY 
 #'''
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimelsa.softbolivia@gmail.com'
-EMAIL_HOST_PASSWORD = 'bgtbwempikmrhyhk'
+EMAIL_HOST_PASSWORD = 'vdcrkktowtctwqtl'
 EMAIL_USE_TLS = True
 #'''
 ######
