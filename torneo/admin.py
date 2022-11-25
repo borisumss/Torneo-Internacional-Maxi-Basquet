e5f9bb4942b09dcc74a96cd2d026d88dbbd221e5
cristian <crisarandosyse@gmail.com>
2022-11-24 21:00:08 -0400

    Update admin.py comments

--- torneo/admin.py
+++ torneo/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-# admin.site.register(Torneo)
-# admin.site.register(Categorias_Torneo)
-# admin.site.register(Organizador)
-# admin.site.register(Equipo)
-# admin.site.register(Entrenador)
-# admin.site.register(Jugador)
-# admin.site.register(Delegado)
-# admin.site.register(Inscripcion)
-# admin.site.register(Pre_Inscripcion)
-# admin.site.register(delegado_Inscripcion)
-# admin.site.register(delegado_PreInscripcion)
-# admin.site.register(Estadisticas_enfrentamiento)
-# admin.site.register(Enfrentamiento)
-
 
 @admin.register(Torneo)
 class Torneo(admin.ModelAdmin):
