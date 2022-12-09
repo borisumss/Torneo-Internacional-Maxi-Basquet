f1c5cb83c08258f5bbc0586d1383d7b249e03f2a
Jheison Leon <201905980@est.umss.edu>
2022-12-09 11:46:55 -0400

    fix date with language=en

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en'
 
 #TIME_ZONE = 'UTC'
 TIME_ZONE = 'America/La_Paz'
