ae593016b8e8aac8af2f26274c00ea8bc183b289
cristian <crisarandosyse@gmail.com>
2022-12-17 14:45:22 -0400

    Update correo smtp

--- torneo_maxi_basquet/settings.py
+++ torneo_maxi_basquet/settings.py
@@ -161,8 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dimelsa.softbolivia@gmail.com'
-EMAIL_HOST_PASSWORD = 'pstbxekgojouqqqf'
+EMAIL_HOST_USER = 'deploy3435@gmail.com'
+EMAIL_HOST_PASSWORD = 'evfnwvyeiarjahob'
 EMAIL_USE_TLS = True
 #'''
 ######
